36c6d7c2f8b5f122019fa190b8bb2ebee520c061
Dev-Hood <estudartagc@gmail.com>
2023-05-22 22:56:53 -0300

    teste

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamentos.component.css']
 })
 export class ListarPensamentosComponent implements OnInit {
-
+ listaPensamentos = [
+  {
+    conteudo : 'I teste angular',
+    autoria: 'Tallys',
+    modelo: 'modelo3'
+  }
+ ]
+  
   constructor() { }
 
   ngOnInit(): void {
